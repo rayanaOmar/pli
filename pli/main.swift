11d4f74b48a44b2817d1409b86c5d966174fd2ad
rayanaOmar <rayanaomar_@hotmail.com>
2021-11-24 12:11:07 +0000

    palindrome

--- pli/main.swift
+++ pli/main.swift
@@ -1,11 +1,21 @@
-//
-//  main.swift
-//  pli
-//
-//  Created by admin on 24/11/2021.
-//
+
 
 import Foundation
 
-print("Hello, World!")
+print("Enter the String for check:")
 
+let userInput = readLine()
+
+if userInput != nil{
+    let result = isPalindrome(s: userInput!)
+    if result {
+        print("\(userInput!) is Palindrome")
+    }else{
+        print("\(userInput!) is Not Palindrome")
+        
+    }
+}
+
+func isPalindrome(s: String) -> Bool{
+    return s == String(s.reversed())
+}
